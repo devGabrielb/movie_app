71dd91edc6d191906cd303a11fe20ea8d85915b8
devGabrielb <55809924+devGabrielb@users.noreply.github.com>
2020-05-16 10:00:13 -0300

    Delete configs.dart

--- lib/configs.dart
+++
@@ -1,6 +0,0 @@
-class Configs{
-
-  static String key="bbf4bf799d935ade2d5f9495d5b74b98";
-  static String baseUrl="https://api.themoviedb.org/3/movie/";
-  static String baseUrlImage="http://image.tmdb.org/t/p/w342";
-}
